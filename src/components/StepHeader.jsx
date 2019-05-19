ab12f425e8c44486b1c2c91d2cf08d0224ecc3d8
JiyarDev <JiyarDev@yahoo.com>
2019-05-20 00:23:26 +0430

    changes

--- src/components/StepHeader.jsx
+++ src/components/StepHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 
 function StepHeader({ step = 3, active = 1 }) {
   const steps = Array.from(Array(step).keys()).map(c => c + 1);
@@ -9,9 +9,7 @@ function StepHeader({ step = 3, active = 1 }) {
       {steps.map((p, k) => {
         let classes = active === p ? `step step-${p} blue` : `step step-${p}`;
         return (
-          <div key={k} className={classes}>
-            {p}
-          </div>
+          <div key={k} className={classes}>{p}</div>
         );
       })}
     </div>
