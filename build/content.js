deb0836bac4e53ff817e611453a2a867cf5810c2
JiyarDev <JiyarDev@yahoo.com>
2019-06-19 17:16:38 +0430

    changes

---
+++ build/content.js
@@ -0,0 +1,24 @@
+/*global chrome*/
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log('content listener');
+  console.log('content Got sender',sender);
+  if (request.message == 'clicked_browser_action') {
+    console.log('Got Message');
+    var myDomOutline = DomOutline({
+      onClick: function(element,xpath) {
+        console.log('Clicked element:', xpath);
+        //localStorage.setItem('xpath', xpath);
+        chrome.storage.local.set({ xpath: xpath });
+        chrome.runtime.sendMessage({ message: 'xpath', inspect: xpath });
+        //sendResponse({ message: element });
+        
+
+      },
+      filter: '*'
+    });
+    myDomOutline.start();
+    //myDomOutline.stop();
+  }
+  //sendResponse({ farewell: "goodbye" });
+});
---
+++ build/precache-manifest.07b0baeb180a70a0016293659212a9b2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "71c1b07cfaa38fff3b5061f46140702a",
+    "url": "/static/media/newTrigger.71c1b07c.svg"
+  },
+  {
+    "revision": "08155d35b3c4e78339cc",
+    "url": "/static/css/app.css"
+  },
+  {
+    "revision": "3e4d22f5ec13e0a445c6",
+    "url": "/static/js/1.chunk.js"
+  },
+  {
+    "revision": "bc6d5ea1ce9e396ff330c328818abf12",
+    "url": "/static/media/logo.bc6d5ea1.svg"
+  },
+  {
+    "revision": "7c542251006e7d69cc5ddeb48d32bb12",
+    "url": "/static/media/buttonClick.7c542251.svg"
+  },
+  {
+    "revision": "c660cc1f317d63ca43dc9405346ea818",
+    "url": "/static/media/viewBtn.c660cc1f.svg"
+  },
+  {
+    "revision": "97503051afa0fa0c25dff1dfed27f696",
+    "url": "/static/media/editBtn.97503051.svg"
+  },
+  {
+    "revision": "cfd9b532281d0d1143abdecf3c32df2d",
+    "url": "/static/media/formSubmit.cfd9b532.svg"
+  },
+  {
+    "revision": "5732d2aff0929c04b0462d1429e747dd",
+    "url": "/static/media/videoPlay.5732d2af.svg"
+  },
+  {
+    "revision": "530dd4f03e87b1a2617d59ce7120e3f3",
+    "url": "/static/media/purchase.530dd4f0.svg"
+  },
+  {
+    "revision": "08155d35b3c4e78339cc",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "3db745f9404c2838228286c585590581",
+    "url": "/static/media/project.3db745f9.svg"
+  },
+  {
+    "revision": "6762ba88a453eef7dce64846fed2c91c",
+    "url": "/static/media/addFieldBtn.6762ba88.svg"
+  },
+  {
+    "revision": "a154a08022a1dedfda5147a4ec0e896b",
+    "url": "/static/media/allProject.a154a080.svg"
+  },
+  {
+    "revision": "f77a8932198879966cb4fb31935d2e02",
+    "url": "/static/media/checkBox.f77a8932.svg"
+  },
+  {
+    "revision": "b7066d14cca17eab9421b31e7876f415",
+    "url": "/static/media/help.b7066d14.svg"
+  },
+  {
+    "revision": "2c0e57a2fc6e15efc137add5301e3111",
+    "url": "/static/media/inputLeft.2c0e57a2.svg"
+  },
+  {
+    "revision": "ccde5e6ba692011112c19e954f2ac7ed",
+    "url": "/static/media/logout.ccde5e6b.svg"
+  },
+  {
+    "revision": "9a5b13f192d1b3a30e2510dae63e1c8b",
+    "url": "/static/media/menu.9a5b13f1.svg"
+  },
+  {
+    "revision": "699f1c2cc9b746cf8972e9bc9fedae23",
+    "url": "/index.html"
+  }
+];
