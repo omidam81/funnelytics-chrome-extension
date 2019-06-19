1e0d3723ea9a1da1e0a27a87366aa599147dcf5c
Omid Amiri <omid.am88@gmail.com>
2019-06-19 06:59:27 +0300

    changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07b0baeb180a70a0016293659212a9b2.js"
+  "/precache-manifest.250241fd91ffe7b2c83f408d18e5d55c.js"
 );
 
 workbox.clientsClaim();
